912ed79792652fee8f7165256a8e3b3cf3b59677
Harsh Verma <harsh99verma8@gmail.com>
2021-10-05 15:28:16 +0530

    remove unnecessary code

--- server/Controller/commit-controller.js
+++ server/Controller/commit-controller.js
@@ -21,25 +21,4 @@ const getCommitDetailsByCommitSHA = (req, res) => {
     });
 };
 
-const getCommitDetailsByCommitSHADiff = (req, res) => {
-
-    var config = {
-      method: "get",
-      url: `https://api.github.com/repos/${req.params.owner}/${req.params.repo}/commits/${req.params.oid}.{diff}`,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-  
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-        return res.status(200).json(response.data)
-      })
-      .catch(function (error) {
-        console.log(error);
-        return res.status(400).json(error)
-      });
-  };
-
 module.exports = { getCommitDetailsByCommitSHA };
